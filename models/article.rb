797d871af3586dfd7a5e9f6c4e2fb57a18fb1515
virgil <virgil.leehong@gmail.com>
2012-11-14 10:26:34 +0800

    update

--- models/article.rb
+++ models/article.rb
@@ -1,8 +1,9 @@
-DB = Sequel.connect("mysql2://sequel:test@localhost:3306/blog")
-
+#DB = Sequel.connect("mysql2://sequel:test@localhost:3306/blog")
+DB = Sequel.sqlite 'blog.db'
 class Article < Sequel::Model
- set_dataset DB[:articles]
- 
+  #set the table name, not used Sequel Model classes
+  # Article.set_dataset :articles  
+
  def date_iso8601
     date_published.strftime("%Y-%m-%dT%H:%M:%SZ")
  end
