16712ce31d11d37f1eb46a1384ee57c27a646c6e
virgil <virgil.leehong@gmail.com>
2012-11-20 11:36:59 +0800

    fix indent

--- models/article.rb
+++ models/article.rb
@@ -9,8 +9,7 @@ class Article < Sequel::Model
    # errors.add(:title, 'cannot be empty') if !Article.title || Article.empty?
   #end
 
-	def date_iso8601
+  def date_iso8601
     date_published.strftime("%Y-%m-%dT%H:%M:%SZ")
- 	end
-
+  end
 end
